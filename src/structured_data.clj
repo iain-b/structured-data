81525c04b19ebd6ccff0dc232efc6f4f5eb5e4e4
ibillett <Iain.Billett@paddypower.com>
2014-07-29 11:57:08 +0100

    Excerises and update git ignore

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,20 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (Math/pow (+ x x) (+ x x))
+)
 
 (defn spiff [v]
-  :-)
+  (+ (get v 0) (get v 2))
+)
 
 (defn cutify [v]
-  :-)
+  (conj v "<8"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[one two three] v]
+    (+ one three))
+)
 
 (defn point [x y]
   [x y])
@@ -19,52 +23,97 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (max (- x1 x2) (- x2 x1))
+  )
+)
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (max (- y1 y2) (- y2 y1))
+  )
+)
 
 (defn square? [rectangle]
-  :-)
+  (== (height rectangle) (width rectangle))
+)
 
 (defn area [rectangle]
-  :-)
+  (* (height rectangle) (width rectangle))
+)
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle
+        [x3 y3] point]
+    (and (<= x1 x3 x2)
+         (<= y1 y3 y2))
+  )
+)
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[[ox1 oy1] [ox2 oy2]] outer
+       [[ix1 iy1] [ix2 iy2]] inner]
+    (and (<= ox1 ix1 ix2 ox2)
+         (<= oy1 iy1 iy2 oy2))
+  )
+)
+
+(def china {:name "China Miéville", :birth-year 1972})
+(def octavia {:name "Octavia E. Butler"
+              :birth-year 1947
+              :death-year 2006})
+(def friedman {:name "Daniel Friedman" :birth-year 1944})
+(def felleisen {:name "Matthias Felleisen"})
+
+(def cities {:title "The City and the City" :authors [china]})
+(def wild-seed {:title "Wild Seed", :authors [octavia]})
+(def embassytown {:title "Embassytown", :authors [china]})
+(def little-schemer {:title "The Little Schemer"
+                     :authors [friedman, felleisen]})
+(def books [cities, wild-seed, embassytown, little-schemer])
 
 (defn title-length [book]
-  :-)
+  (count (:title book))
+)
 
 (defn author-count [book]
-  :-)
+  (count (:authors book))
+)
 
 (defn multiple-authors? [book]
-  :-)
+  (>= (author-count book) 2)
+)
 
 (defn add-author [book new-author]
-  :-)
+    (assoc book :authors (conj (:authors book) new-author))
+)
 
 (defn alive? [author]
-  :-)
+  (not (contains? author :death-year))
+)
 
 (defn element-lengths [collection]
-  :-)
+  (map count collection))
 
 (defn second-elements [collection]
-  :-)
+  (let [second-element (fn [element] (second element))]
+    (map second-element collection)
+  )
+)
 
 (defn titles [books]
-  :-)
+  (map :title books))
 
 (defn monotonic? [a-seq]
-  :-)
+  (or
+    (apply <= a-seq)
+    (apply >= a-seq)
+  )
+)
 
 (defn stars [n]
-  :-)
+  (apply str (repeat n \*))
+)
 
 (defn toggle [a-set elem]
   :-)
